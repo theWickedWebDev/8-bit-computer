168d13da1455ffef5bba3a0c1bbbfb82f2436e0b
Stephen Young <stephenyoung@Stephens-MacBook-Pro.local>
2021-11-28 01:41:57 -0500

    updated webapp

--- WebApp/src/App.js
+++ WebApp/src/App.js
@@ -1,19 +1,45 @@
-import { Table, Tag, Typography } from 'antd';
+import { Layout, Menu, Breadcrumb, Table, Typography } from 'antd';
+import { useState } from 'react';
 import opcodes from './data/opcodes';
 import { opcodeTableCols } from './tables/opcode';
-
+const { Header, Content, Footer } = Layout;
 const { Text } = Typography;
 
 function App() {
+  const [page, setPage] = useState("opcodes");
+
+  let mainContent = <div/>
+  if (page === 'opcodes') {
+    mainContent = (
+    <Table
+      columns={opcodeTableCols}
+      dataSource={opcodes}
+      pagination={false}
+    />
+    );
+  }
+
+  if (page === 'instructions') {
+    mainContent = (
+      <h1>Instructions</h1>
+    );
+  }
 
   return (
-    <div className="App">
-      <Table
-        columns={opcodeTableCols}
-        dataSource={opcodes}
-        pagination={false}
-      />
-    </div>
+    <Layout className="layout">
+    <Header>
+      <Menu onSelect={({ key }) => setPage(key)} theme="dark" mode="horizontal" defaultSelectedKeys={[page]}>
+        <Menu.Item key="instructions">Instructions</Menu.Item>
+        <Menu.Item key="opcodes">OpCodes</Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{ padding: '0 50px' }}>
+      <div className="site-layout-content">
+        {mainContent}
+      </div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+  </Layout>
   );
 }
 
